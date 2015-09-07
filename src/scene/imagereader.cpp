2147f297b7ad034303c56f8f93232f192a692835
Mark Reid <mindmark@gmail.com>
2015-09-06 22:27:36 -0700

    removed ImageReader class

--- src/scene/imagereader.cpp
+++ src/scene/imagereader.cpp
@@ -10,21 +10,10 @@
 #include <list>
 #include <chrono>
 
-ImageReader::ImageReader()
+
+void setup_imagemagick()
 {
     Magick::InitializeMagick(NULL);
-    m_loading = false;
-    m_requested_time = 0;
-}
-
-
-static bool load(ImageReader *reader)
-{
-    //std::cerr << "loading image bg in thread\n";
-    bool result = reader->loadData();
-    //std::cerr << "LOADDDED FULL image in bg thread\n";
-    return result;
-
 }
 
 void read_image(const std::string &path,
@@ -146,69 +135,4 @@ void write_template_psd(const std::string &imageplane,
     images.push_back(contour);
     p.set_value(90);
     Magick::writeImages(images.begin(), images.end(), "out.psd");
-}
-
-void ImageReader::read(const std::string &path, double time)
-{
-    std::lock_guard<std::recursive_mutex> lock(m_lock);
-    setPath(path, time);
-
-    if(!m_loading) {
-        m_loading = true;
-        m_loading_time = time;
-        m_future = std::async(std::launch::async, load, this);
-    }
-
-}
-bool ImageReader::loadData()
-{
-    Magick::Image image;
-    std::chrono::time_point<std::chrono::system_clock> start;
-    start = std::chrono::system_clock::now();
-
-    try {
-        image.read( path());
-    }
-    catch (...) {
-        std::cerr << "error reading" << path() << "\n";
-        //return false;
-        image.read("logo:");
-    }
-    image.resize("1024x1024!");
-    int image_width =image.size().width();
-    int image_height = image.size().height();
-    // std::cerr << image_width << "x" << image_height << "\n";
-    std::lock_guard<std::recursive_mutex> lock(data_lock);
-    setWidth(image_width);
-    setHeight(image_height);
-    image_data.resize(image_width * image_height * 4 );
-    image.write(0, 0, image_width, image_height, "RGBA",  Magick::FloatPixel, &image_data[0]);
-
-    std::chrono::duration<double> elapsed_seconds = std::chrono::system_clock::now()-start;
-    std::cerr << "image read in " << elapsed_seconds.count() << " secs\n";
-
-    return true;
-}
-void ImageReader::requeue()
-{
-    std::lock_guard<std::recursive_mutex> lock(m_lock);
-    read(m_path, m_requested_time);
-}
-
-bool ImageReader::dataReady(double &time)
-{
-    std::lock_guard<std::recursive_mutex> lock(m_lock);
-    if (!m_loading)
-        return false;
-
-    std::future_status status;
-    status = m_future.wait_for(std::chrono::microseconds(1));
-
-    if (status ==  std::future_status::ready){
-        m_loading = false;
-        time = m_loading_time;
-        return m_future.get();
-    }
-
-    return false;
 }
--- src/scene/imagereader.h
+++ src/scene/imagereader.h
@@ -33,6 +33,7 @@ private:
 
 };
 
+void setup_imagemagick();
 
 void read_image(const std::string &path,
                 std::vector<float> &data,
@@ -47,71 +48,5 @@ void write_template_psd(const std::string &imageplane,
                         FloatImageData &alpha_data,
                         FloatImageData &contour_data,
                         Progress &p);
-
-class ImageReader
-{
-public:
-    ImageReader();
-
-    void read(const std::string &path, double time);
-    bool dataReady(double &time);
-    std::vector<float> image_data;
-    int width()
-    {
-        std::lock_guard<std::recursive_mutex> lock(m_lock);
-        return m_width;
-    }
-    void setWidth(int width)
-    {
-        std::lock_guard<std::recursive_mutex> lock(m_lock);
-        m_width = width;
-    }
-
-    int height()
-    {
-        std::lock_guard<std::recursive_mutex> lock(m_lock);
-        return m_height;
-    }
-
-    void setHeight(int height)
-    {
-        std::lock_guard<std::recursive_mutex> lock(m_lock);
-        m_height = height;
-    }
-
-    void setPath(const std::string &path, double time) {
-         std::lock_guard<std::recursive_mutex> lock(m_lock);
-         m_requested_time = time;
-         m_path = path;
-    }
-    void requeue();
-    std::string path() {
-        std::lock_guard<std::recursive_mutex> lock(m_lock);
-        return m_path;
-    }
-    void clear()
-    {
-        std::lock_guard<std::recursive_mutex> lock(data_lock);
-        image_data.clear();
-    }
-
-    bool loadData();
-    std::recursive_mutex data_lock;
-
-private:
-
-    bool m_loading;
-    double m_loading_time;
-    double m_requested_time;
-
-    int m_width;
-    int m_height;
-
-    std::string m_path;
-    std::future<bool> m_future;
-    std::recursive_mutex m_lock;
-
-};
-
 
 #endif // IMAGEREADER_H
